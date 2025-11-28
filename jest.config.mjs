6c66f88e7846fa65cdfc16d5c2fdec61cf586f35
Nhi Nguyen <lamnhi.ngngoc@gmail.com>
2025-11-28 13:15:00 +0200

    Update

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,7 @@
 export default {
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/tests/**/example.test.js'],
+  transform: {},
+  testMatch: ['<rootDir>/tests/**/*.test.js'],
   collectCoverageFrom: [
     'src/**/*.js',
     '!src/.internal/**',
